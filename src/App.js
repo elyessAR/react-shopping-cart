6671c89aad14938db37470a320d3660a39141bc2
lyess123 <49523731+lyess123@users.noreply.github.com>
2020-09-11 02:22:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+     <div className="grid-container">
+       <header>
+         <a href="/"> React shopping cart </a>
+       </header>
+       <main>
+         Product List
+       </main>
+       <footer>
+         All right is reserved 
+       </footer>
+      </div>
+        
+
+
+
+  );
+}
+
+export default App;
